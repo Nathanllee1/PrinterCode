18b36af57a014fbee65becfcebf2324541950827
Nathan <nathan.l.lee123@gmail.com>
2020-08-24 05:40:09 +0100

    Added file logging for easier debuggin

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ var credentials = require("./credentials.json")
 require("firebase/auth");
 require("firebase/firestore");
 
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
 const init = () => {firebase.initializeApp({
   apiKey: "AIzaSyAsTXK6zgFSmJ3wrkc3fa1KOaJMPWV97Rs",
   authDomain: "print-chatter.firebaseapp.com",
@@ -45,8 +54,9 @@ const queryDown = (db) => {
         printImage(data.Data, 'pictures/' + filename)
         const id = change.doc.id
 
-        db.collection('Messages').doc(id).set({
-          printed: true
+        db.collection('Messages').doc(id).update({
+          Printed: true,
+
         })
       }
     })
